46d9b896759533f06f222c0056ed0b634f183f34
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-04-12 23:40:15 -0500

    Cambio en package.json y favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'iVueMusic',
+    title: 'iVuesic',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'The best Music' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/music.png' }
     ]
   },
   /*
